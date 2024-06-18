da9050fa204f8944b1bd1507fbd330358b30abda
Alexander Gordienko <reclaite@gmail.com>
2024-06-18 15:17:12 +0300

    Реализован новый контракт для отсылки от скетча - изменена некоторая логика работы и удалены лишние неиспользумые константы - частичный рефакторинг кода - добавление контракта истории записей

--- arduino.ino
+++ arduino.ino
@@ -3,120 +3,129 @@
 #include <Wire.h>
 #include <SoftwareSerial.h>
 #include <MQ135.h>
-#include <math.h>
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);  // Устанавливаем дисплей
 #define DHTPIN 5
 #define DHTTYPE DHT11
+#define BUZZER 4
+#define CACHE_SIZE 10
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);  // Устанавливаем дисплей
 DHT dht(DHTPIN, DHTTYPE);
 MQ135 gasSensor(A3);
-int buzzer = 4;
-unsigned long previousMillis = 0;
+SoftwareSerial BTSerial(10, 11);
+
 const long interval = 5000;
 bool isHumidityDisplayed = false;
-SoftwareSerial BTSerial(10, 11);
 
 float prevTemp, prevHum, prevGasPPM;
 
+// Контракт обмена сообщениями размера CACHE_SIZE
+// "{temperature}#{humidity}#{ppm}", значения приходящего кеша разделяются ","
+String outputCache[CACHE_SIZE];
+
 void setup() {
-  lcd.begin(16, 2);
-  lcd.init();
-  lcd.backlight();
-  dht.begin();
-  Serial.begin(9600);
-  BTSerial.begin(9600);
-  MQ135 gasSensor = MQ135(A0);
+    lcd.begin(16, 2);
+    lcd.init();
+    lcd.backlight();
+    dht.begin();
+    Serial.begin(9600);
+    BTSerial.begin(9600);
+    MQ135 gasSensor = MQ135(A0);
 }
+
 void loop() {
-  float temp = dht.readTemperature();
-  float hum = dht.readHumidity();
-  int gasValue = gasSensor.getPPM();
-  unsigned long currentMillis = millis();
+    float temp = dht.readTemperature();
+    float hum = dht.readHumidity();
+    int gasValue = gasSensor.getPPM();
 
-  if (isnan(temp)) {
-    temp = prevTemp;
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Temp: ");
-    lcd.print(temp);
-    lcd.print("C");
+    if (isnan(temp)) {
+        temp = prevTemp;
+        displayInformation(0, 0, concat("Temp: ", temp, "C"));
 
-    BTSerial.print("Temperature: ");
-    BTSerial.print(temp);
-    BTSerial.println(" C");
-  } else {
-    if (temp != prevTemp) {
-      prevTemp = temp;
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("Temp: ");
-      lcd.print(temp);
-      lcd.print("C");
-
-      BTSerial.print("Temperature: ");
-      BTSerial.print(temp);
-      BTSerial.println(" C");
-    }
-  }
-  if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
-    if (isHumidityDisplayed) {
-      if (isnan(hum)) {
-        hum = prevHum;
-        lcd.setCursor(0, 2);
-        lcd.print("Humidity: ");
-        lcd.print(hum - 10);
-        lcd.print("%");
-
-        BTSerial.print("Humidity: ");
-        BTSerial.print(hum);
-        BTSerial.println(" %");
-        isHumidityDisplayed = false;
-      } else {
-        if (hum != prevHum) {
-          prevHum = hum;
-          lcd.setCursor(0, 1);
-          lcd.print("Humidity: ");
-          lcd.print(hum - 10);
-          lcd.print("%");
-
-          BTSerial.print("Humidity: ");
-          BTSerial.print(hum);
-          BTSerial.println(" %");
-          isHumidityDisplayed = false;
+        BTSerial.print("Temperature: ");
+        BTSerial.print(temp);
+        BTSerial.println(" C");
+    } else {
+        if (temp != prevTemp) {
+            prevTemp = temp;
+            displayInformation(0, 0, concat("Temp: ", temp, "C"));
         }
-      }}else {
-        if (gasValue > 100 && gasValue != prevGasPPM) {
-          prevGasPPM = gasValue;
-          lcd.setCursor(0, 1);
-          lcd.print("CO2: ");
-          lcd.print(prevGasPPM + 300);
-          lcd.print("  PPM");
+    }
 
-          BTSerial.print("Gas Level: ");
-          BTSerial.print(prevGasPPM);
-          BTSerial.println(" PPM");
-          isHumidityDisplayed = true;
+    if (isHumidityDisplayed) {
+        if (isnan(hum)) {
+            hum = prevHum;
+            displayInformation(0, 2, concat("Humidity: ", hum - 10, "%"));
+            isHumidityDisplayed = false;
+        } else {
+            if (hum != prevHum) {
+                prevHum = hum;
+                displayInformation(0, 1, concat("Humidity: ", hum - 10, "%"));
+                isHumidityDisplayed = false;
+            }
+        }
+    } else {
+        if (gasValue > 100 && gasValue != prevGasPPM) {
+            prevGasPPM = gasValue;
+            displayInformation(0, 1, concat("CO2: ", prevGasPPM + 300, "ppm"));
+            isHumidityDisplayed = true;
         }
         if (prevGasPPM + 300 > 100) {
-          tone(buzzer, 100);
-          delay(500);
-          noTone(buzzer);
-          isHumidityDisplayed = true;
+            tone(BUZZER, 100);
+            delay(500);
+            noTone(BUZZER);
+            isHumidityDisplayed = true;
         }
-        Serial.print("Temperature: ");
-        Serial.print(temp);
-        Serial.println(" C");
 
-        Serial.print("Humidity: ");
-        Serial.print(hum);
-        Serial.println(" %");
-
-        Serial.print("Gas Level: ");
-        Serial.print(prevGasPPM);
-        Serial.println(" PPM");
-
-        delay(4000);  // Задержка между чтениями
-      }
+        Serial.println(concat("Temperature: ", temp, " C"));
+        Serial.println(concat("Humidity: ", hum, " %"));
+        Serial.println(concat("Gas Level: ", gasValue, " PPM"));
     }
-  }
+
+    String currentResult = getCurrentOutput(temp, hum, gasValue);
+    updateCache(currentResult);
+    Serial.println(currentResult);
+    BTSerial.println(getOverallOutput());
+
+    delay(interval);  // Задержка между чтениями
+}
+
+// Отображение какого-либо контента на LCD
+void displayInformation(int offsetX, int offsetY, String content) {
+    lcd.clear();
+    lcd.setCursor(offsetX, offsetY);
+    lcd.print(content);
+}
+
+// Получает текущий контрактный вывод информации вида {temperature}#{humidity}#{ppm}
+String getCurrentOutput(float temperature, float humidity, int gasValue) {
+    return concat(temperature, "#", humidity, "#", gasValue);
+}
+
+// Получает общий вывод по кешу, разделяя его ","
+String getOverallOutput() {
+    String cacheDump = "";
+    for (int i = 0; i < sizeof(outputCache); i++) {
+        cacheDump = concat(cacheDump, outputCache[i], ",");
+    }
+    return cacheDump;
+}
+
+// Обновляет кеш, постепенно избавляясь от самых старых значений
+// Добавляет новое значение в самое начало
+void updateCache(String newOutput) {
+    for (int i = sizeof(outputCache) - 2; i >= 0; i--) {
+        outputCache[i + 1] = outputCache[i];
+    }
+    outputCache[0] = newOutput;
+}
+
+// Костыль, чтобы конкатенировать примитивы помимо строк
+// (по умолчанию необходимо приводить float/int/byte... в String(float), а только потом можно конкатенироваться)
+String concat() {
+    return "";
+}
+template<typename T, typename... Args>
+String concat(T first, Args... args) {
+    return String(first) + concat(args...);
+}
